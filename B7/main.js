80541e8b1238e3c4f98ecf39ed30395418f14385
Tai Nguyen Tan <machxukilami@gmail.com>
2022-05-09 22:44:48 +0700

    bt-b7

---
+++ B7/main.js
@@ -0,0 +1,17 @@
+// Bai1 in ra cac thang trong nam
+document.write("Bài 1" + "<br/>")
+
+for(i = 1; i <= 12; i++) {
+    document.write('thang ' + i + "<br/>");
+}
+
+document.write("Bài 3" + "<br/>")
+
+// Bai3
+for(i = 1; i <= 9; i++) {
+    for( j = 1; j <= 10; j++) {
+        document.write(i + " x " + j + " = " + i*j + "<br/>");
+    }
+}
+
+
