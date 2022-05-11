b4d8fc4f6101095642886f9251a778be5ff09f04
Tai Nguyen Tan <machxukilami@gmail.com>
2022-05-12 01:10:20 +0700

    bt-b8

---
+++ B8/main.js
@@ -0,0 +1,73 @@
+
+
+// bài 1
+
+// let pheptoan = prompt('nhap vao phep toan an muon')
+// let a = Number(prompt('nhap vao so dau tien'))
+// let b = Number(prompt('nhap vao so thu 2'))
+// let result = 0;
+// if ( pheptoan == "+" || pheptoan == "cộng" || pheptoan == "cong") {
+//     result = a + b;
+//     alert(`tổng ${a} + ${b} băng ${result}`)
+// }else if ( pheptoan == "-" || pheptoan == "trừ" || pheptoan == "tru") {
+//     result = a - b 
+//     alert(`hiệu ${a} và ${b} băng ${result}`)
+// }else if ( pheptoan == "x" || pheptoan == "nhân" || pheptoan == "nhan") {
+//     result = a*b
+//     alert(`tích ${a} nhân ${b} băng ${result}`)
+// }else if ( pheptoan == "/" || pheptoan == "chia" || pheptoan == "chia") {
+//     result = a / b
+//     alert(` ${a} chia ${b} băng ${result}`)
+// } else {
+//     alert('nhập lại phép tính nhé')
+// }
+
+
+
+// bài2
+
+// kiểm tra số nguyên tố
+
+// let nhapso = Number(prompt('nhập 1 số đi anh bạn'))
+
+// let sonhap = Number(prompt('nhập 1 số nào anh bạn'));
+// let soNguyenTo = true;
+// for (i = 2; i < sonhap; i++) {
+//     if (sonhap % i == 0) {
+//         soNguyenTo = false ;
+//         break;
+//     }
+// }
+// if (soNguyenTo == true) {
+//     alert(`${sonhap} là số nguyên tố`)
+// } else {
+//     alert(`${sonhap} ko phải là số nguyên tố`)
+// }
+
+
+
+// in ra số nguyên tố từ 1 đến n
+
+// function checksonguyento(so) {
+//     soNguyenTocheck = 1;
+//     if(so < 2) {
+//         soNguyenTocheck = 0
+//     }
+//     for(i = 2; i < so; i++) {
+//         if(so % i == 0) {
+//             soNguyenTocheck = 0
+//         }
+//     }
+//     return soNguyenTocheck;
+    
+// }
+
+// for(i = 0; i < sonhap; i++) {
+//     if(checksonguyento(i) == 1) {
+//         console.log(i);
+//     }
+// }
+
+
+
+
