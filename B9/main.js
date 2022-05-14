2f7502fbcf031b82b1ac796eaaed1e455546a176
Tai Nguyen Tan <machxukilami@gmail.com>
2022-05-14 14:57:24 +0700

    bt-b9

---
+++ B9/main.js
@@ -0,0 +1,45 @@
+// bài1
+// function checkAge(age) {
+//     return (age > 18) ? true : confirm('Did parents allow you?')
+// }
+
+// bài 2
+// let a = Number(prompt('nhập số a'))
+// let c = Number(prompt('nhập số c'))
+// function checkmin(e, f) {
+//     ketqua = ""
+//     if (e > f) {
+//         ketqua = ketqua + `${f} là số nhỏ nhất`
+//         return alert(ketqua)
+//     } else {
+//         ketqua = ketqua + `${e} là số nhỏ nhất`
+//         return alert(ketqua)
+//     }
+// }
+// checkmin(a, b)
+
+// bài3
+
+
+// ko hiểu đề bài yêu cầu gì?
+
+// bài 4
+// let account1 = prompt('tên tài khoản')
+
+// function checkLogin(account) {
+//     if (account == "Admin") {
+//         let pass = prompt('mật khẩu')
+//         if (pass == 'mindX') {
+//             alert("chào mừng")
+//         }
+//         else if (pass == null || pass == "") {
+//             alert('Đã hủy')
+//         }
+//         else if(pass !== "mindX") {
+//             alert('sai mật khẩu')
+//         }
+//     } else {
+//         alert('sai tên đăng nhập')
+//     }
+// }
+// checkLogin(account1)
