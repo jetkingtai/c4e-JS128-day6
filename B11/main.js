285980b99d1889e60695c43eb7142fffc5fbc244
admin <admin@VL-HCM-LAP-MACVFVH8.local>
2022-05-19 11:59:29 +0700

    bt-b11

---
+++ B11/main.js
@@ -0,0 +1,26 @@
+// --------- bài 1 -----------
+// let nut1 = document.querySelector("#nut1");
+// console.log(nut1);
+// nut1.textContent = "Nhan"
+
+// ------------ bài 2 -----------
+// let tieuDe = document.querySelector("#tieudeh1")
+// tieuDe.innerText = "Hello Word" 
+// let inner = tieuDe.innerText;
+
+// tieuDe.innerHTML = `${inner} <input placeholder="nhập vào đây"  type="text">`
+
+// tieuDe.style.background = 'blue'
+
+
+// Bài 3
+
+// let list = document.querySelector("#myList")
+
+// function revome() {
+//     list.removeChild(list.firstElementChild)
+// }
+// let change = document.querySelector("body")
+// function changeC () {
+//     change.style.backgroundColor = "#"+((1<<24)*Math.random()|0).toString(16)
+}
